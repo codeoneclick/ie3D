8bed960077c630d10c6deecb0da18a1565162b4f
Sergey Sergeev <sergey.sergeev.oneclick@gmail.com>
2013-08-19 22:45:30 +0300

    small refactoring

--- indie2dEngine-Core/Sources/IResourceLoadingHandler.h
+++ indie2dEngine-Core/Sources/IResourceLoadingHandler.h
@@ -6,9 +6,59 @@
 //  Copyright (c) 2013 Sergey Sergeev. All rights reserved.
 //
 
-#ifndef __gEngine_Core__IResourceLoadingHandler__
-#define __gEngine_Core__IResourceLoadingHandler__
+#ifndef IResourceLoadingHandler_h
+#define IResourceLoadingHandler_h
 
-#include <iostream>
+#include "HCommon.h"
 
-#endif /* defined(__gEngine_Core__IResourceLoadingHandler__) */
+class CResourceLoadingCommand final
+{
+private:
+    
+    typedef std::function<void(f32)> __SCENE_UPDATE_COMMAND;
+    
+    friend class ISceneUpdateHandler;
+    friend class CSceneUpdateMgr;
+    
+    __SCENE_UPDATE_COMMAND m_sceneUpdateCommand;
+    
+protected:
+    
+    CSceneUpdateCommands(void);
+    
+    void _ConnectSceneUpdateCommand(const __SCENE_UPDATE_COMMAND& _command);
+    void _ExecuteSceneUpdateCommand(f32 _deltatime);
+    
+public:
+    
+    ~CSceneUpdateCommands(void);
+};
+
+class ISceneUpdateHandler
+{
+private:
+    
+    friend class CSceneUpdateMgr;
+    
+    CSceneUpdateCommands m_commands;
+    
+    void _ConnectCommands(void);
+    inline CSceneUpdateCommands _Get_Commands(void)
+    {
+        return m_commands;
+    };
+    
+protected:
+    
+    ISceneUpdateHandler(void);
+    
+    virtual void _OnSceneUpdate(f32 _deltatime) = 0;
+    
+public:
+    
+    virtual ~ISceneUpdateHandler(void);
+};
+
+
+
+#endif 
