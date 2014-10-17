a347e09a12e66c9c16f17e0529cddd7ec232a3ca
sergey.sergeev <codeoneclick@Mac-mini-Valeriy.local>
2014-10-17 18:07:58 +0300

    start implementation of landscape seams

---
+++ ie3D-Core/Sources/CLandscapeSeams.cpp
@@ -0,0 +1,9 @@
+//
+//  CLandscapeSeams.cpp
+//  ie3D-Core
+//
+//  Created by sergey.sergeev on 10/17/14.
+//  Copyright (c) 2014 Sergey Sergeev. All rights reserved.
+//
+
+#include "CLandscapeSeams.h"
