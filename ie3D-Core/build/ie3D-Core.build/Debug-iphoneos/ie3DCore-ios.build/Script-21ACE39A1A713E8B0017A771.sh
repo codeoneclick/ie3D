7e3d4fa154ba526a1b1c8a5128dab162c5490056
codeoneclick <sergey.sergeev.oneclick@gmail.com>
2015-04-15 15:23:39 +0300

    updated QT version to 5.4

---
+++ ie3D-Core/build/ie3D-Core.build/Debug-iphoneos/ie3DCore-ios.build/Script-21ACE39A1A713E8B0017A771.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+#python "${SRCROOT}/../ie3D-Scripts/CONFIGURATION_GENERATOR.py" current_accessor_CConfigurationAccessor declaration_HConfigurationDeclarations
---
+++ ie3D-Engine/build/ie3D-Engine.build/Debug-iphoneos/ie3DEngine-ios.build/Script-21CB354E1A7D1C0000819C09.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+#python "${SRCROOT}/../ie3D-Scripts/CONFIGURATION_GENERATOR.py" current_accessor_CEConfigurationAccessor base_accessor_CConfigurationAccessor declaration_HEConfigurationDeclarations
