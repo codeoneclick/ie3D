fc3779d97550051b0e1989f351d0b433cac18073
codeoneclick <sergey.sergeev.oneclick@gmail.com>
2015-04-20 16:59:32 +0300

    updated gitignore

--- ie3D-EMap/ie3D-EMap-IOS.build/Debug-iphonesimulator/Qt Preprocess.build/Script-5E618435888B9D49F8540165.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/codeoneclick/Documents/hobby/ie3D/ie3D-EMap -f ie3D-EMap-IOS.xcodeproj/qt_makeqmake.mak
--- ie3D-EMap/ie3D-EMap-IOS.build/Debug-iphonesimulator/ie3D-EMap.build/Script-3DA90DAE39A0C9D07C6F6B0F.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-/Volumes/media/Applications/QT/5.4/ios/mkspecs/macx-ios-clang/rename_main.sh ${OBJECT_FILE_DIR}-${CURRENT_VARIANT} "${ARCHS}"
